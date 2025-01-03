172d2de4cbef1824393725a7e541d4470e24110d
tmbv93 <tomasbv@hey.com>
2025-01-03 16:57:09 +0100

    And schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_01_01_211641) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_01_211641) do
   create_table "invites", force: :cascade do |t|
     t.integer "meeting_id", null: false
     t.integer "membership_id", null: false
