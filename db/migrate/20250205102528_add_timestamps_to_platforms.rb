db819c262b55dfc999899793f8f90be68626a74a
tmbv93 <tomasbv@hey.com>
2025-02-05 14:11:06 +0100

    Migration run, remove TODO

--- db/migrate/20250205102528_add_timestamps_to_platforms.rb
+++ db/migrate/20250205102528_add_timestamps_to_platforms.rb
@@ -1,10 +1,5 @@
   class AddTimestampsToPlatforms < ActiveRecord::Migration[8.0]
     def change
-      add_timestamps :platforms, null: true
-
-      # TODO :Remove these three statements and null: true above, once migration has run on server
-      Platform.update_all(created_at: Time.current, updated_at: Time.current)
-      change_column_null :platforms, :created_at, false
-      change_column_null :platforms, :updated_at, false
+      add_timestamps :platforms
     end
   end
