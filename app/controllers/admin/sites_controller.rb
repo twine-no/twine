7b9a790e8519c4ecfe9fa648e05f8ddbc4895306
tmbv93 <tomasbv@hey.com>
2025-02-07 12:25:53 +0100

    Resize images on upload

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -1,6 +1,9 @@
 module Admin
   class SitesController < AdminController
-    before_action :set_platform, only: [ :show, :update ]
+    before_action :set_platform, only: [:show, :update]
+    before_action lambda {
+      resize_logo_file(platform_params[:logo], width: 200, height: 200)
+    }, only: [:update]
 
     def show
     end
@@ -21,6 +24,15 @@ module Admin
 
     def platform_params
       params.require(:platform).permit(:logo, :name, :tagline, :shortname, :listed)
+    end
+
+    def resize_logo_file(image_param, width:, height:)
+      return unless image_param
+
+      ImageProcessing::MiniMagick
+        .source(image_param)
+        .resize_to_fit(width, height)
+        .call(destination: image_param.tempfile.path)
     end
   end
 end
