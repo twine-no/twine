62b41da377c1e26f7c0fe9afdce5cbf14aad8cc6
tmbv93 <tomasbv@hey.com>
2026-04-06 10:44:16 +0200

    Update assessment in a transaction

--- app/controllers/users/memberships/assessments_controller.rb
+++ app/controllers/users/memberships/assessments_controller.rb
@@ -1,20 +1,30 @@
 module Users
   module Memberships
     class AssessmentsController < ApplicationController
+      before_action :set_membership
+
       def create
-        membership = Current.user.memberships.find(params[:membership_id])
         value = params[:value].to_i.clamp(0, 100)
-        membership.assessments.create!(value: value)
-        membership.update!(feeling: value)
+
+        @membership.transaction do
+          @membership.assessments.create!(value: value)
+          @membership.update!(feeling: value)
+        end
 
         Turbo::StreamsChannel.broadcast_replace_to(
-          [ Current.user, :memberships ],
-          target: ActionView::RecordIdentifier.dom_id(membership, :assessment),
+          [Current.user, :memberships],
+          target: ActionView::RecordIdentifier.dom_id(@membership, :assessment),
           partial: "users/assessments/slider",
           locals: { membership: membership }
         )
 
         head :ok
+      end
+
+      private
+
+      def set_membership
+        @membership = Current.user.memberships.find(params[:membership_id])
       end
     end
   end
